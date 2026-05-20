5933629d5040b7b5aff9746d699f525b9acf349f
nebi.uo <nebiyoutahoa@gmail.com>
2026-05-20 11:34:42 +0300

    Implemented Addition and Subtraction Methods

--- Vector_Engine.java
+++ Vector_Engine.java
@@ -1,5 +1,66 @@
-public class Vector_Engine{
-    public static void main (String[] args) {
+import java.util.ArrayList;
+import java.util.Arrays;
 
+public class Vector_Engine{
+
+    public static double[] add(double[] u, double[] v) {
+
+        double[] sum = new double[u.length];
+        int vectorLength = u.length;
+        
+        if (u.length != v.length) {
+
+            double[] empty = {};
+
+            System.out.println("Invalid Operation");
+            return empty;
+
+        }
+        else{
+
+            for(int i = 0; i < vectorLength; i++){
+                sum[i] = u[i] + v[i];
+            }
+
+            return sum;
+        }
     }
+
+    public static double[] subtract(double[] u, double[] v) {
+
+        double[] difference = new double[u.length];
+        int vectorLength = u.length;
+
+        if (u.length != v.length) {
+
+            double[] empty = {};
+
+            System.out.println("Invalid Operation");
+            return empty;
+
+        }
+        else {
+            
+            for (int i = 0; i < vectorLength; i++) {
+                difference[i] = u[i] - v[i];
+            }
+
+            return difference;
+
+        }
+    }
+
+    // public static double[] 
+
+    public static void main (String[] args) {
+        double[] v1 = {2, 4, 3};
+        double[] v2 = {3, 4, 2};
+
+        Vector_Engine vecOp = new Vector_Engine();
+        double[] vsum = vecOp.add(v1, v2);
+        double[] vdiff = vecOp.subtract(v1 , v2);
+
+        System.out.println(Arrays.toString(vsum));
+        System.out.println(Arrays.toString(vdiff));
+    }   
 }
